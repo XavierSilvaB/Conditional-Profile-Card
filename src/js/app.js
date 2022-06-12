f9f6554cf089ec6eff98f0039f5040b1db389589
XavierSilvaB <xavieraugustosilvabaute@gmail.com>
2022-06-12 23:02:08 +0000

    Ejercicio de Conditional Profile Card

--- src/js/app.js
+++ src/js/app.js
@@ -33,10 +33,14 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${variables.name == null ? " " : variables.name} ${
+    variables.lastname == null ? " " : variables.lastname
+  }</h1>
+  <h2>${variables.role ? variables.role : " "}</h2>
+  <h3>${variables.city ? variables.city : " "},${
+    variables.country ? variables.country : " "
+  }</h3>
+          <ul class=${variables.socialMediaPosition}>
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
@@ -61,7 +65,7 @@ window.onload = function() {
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: null,
     linkedin: null,
     instagram: null,
     name: null,
